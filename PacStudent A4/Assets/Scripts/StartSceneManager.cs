89b330a868263c33483ff6fb6b20da9001cfdd5c
Roboshad20 <roboshad20o5@gmail.com>
2025-10-28 04:46:36 +1100

    Started to make the UI Start Screen

---
+++ PacStudent A4/Assets/Scripts/StartSceneManager.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartSceneController : MonoBehaviour
+{
+    public void LoadLevel1()
+    {
+        SceneManager.LoadScene("PlaceHolderScene");
+    }
+
+    public void LoadLevel2()
+    {
+    
+    }
+}
