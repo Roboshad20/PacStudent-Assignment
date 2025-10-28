c8331dcec4fa0fb1cfa1c4936fb624a700fdbdf7
Roboshad20 <roboshad20o5@gmail.com>
2025-10-28 15:37:48 +1100

    Making the HUD of the main game

--- PacStudent A4/Assets/Scripts/StartSceneManager.cs
+++ PacStudent A4/Assets/Scripts/StartSceneManager.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class StartSceneController : MonoBehaviour
+public class StartSceneManager : MonoBehaviour
 {
     public void LoadLevel1()
     {
